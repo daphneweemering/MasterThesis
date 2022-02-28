09d7d71d6d0620f9f422cfc21ee6d771696d3314
daphneweem <dnweemering@gmail.com>
2022-02-28 10:43:03 +0100

    Changed the comments

--- simulations/Thesis/Simulation sample size reestimation n-of-1.R
+++ simulations/Thesis/Simulation sample size reestimation n-of-1.R
@@ -1,26 +1,42 @@
-# 'Interim sample size reestimation for adequately powered
-# series of N-of-1 trials' 
+# 'Interim sample size reestimation for adequately powered series of N-of-1 trials' 
 
 library(lme4)
 library(pwr)
 library(lmerTest)
 
-# Specify the values for \sigma^2 (= 'varerror), \psi^2 (= 'vartreatment') and 
-# the fraction of the initial sample size on which reestimation will be based, fn
-# (= 'fnt')
+# Specify the values
 hvar_treatment <- c(0.5, 1, 2)
 hvar_error <- c(0.25, 0.5, 1)
 tvar_treatment <- c(0.5, 1, 2)
 tvar_error <- c(0.25, 0.5, 1)
 fn <- c(0.25, 0.5, 0.75)
 
-# Create data frame including all possible combinations of \sigma^2, \psi^2 and 
-# fn
+# Create data frame including all possible combinations of 'hvar_treatment', 
+# 'hvar_error', 'tvar_treatment', tvar_error' and 'fn'
 x <- expand.grid(hvar_treatment, hvar_error, tvar_treatment, tvar_error, fn)
 
-# Make storage
-#initial <- matrix(data = NA, nrow = 4, ncol = 243)
+x1 <- x[-c(1,2,3,4,5,6,7,8,9,10),]
 
-# Use apply to calculate the initial sample size for every combination of 
-# hypothesized \sigma^2 and \psi^2 and the fraction of the sample size (fn)
+# Use apply to use the function 'reestim' from the file 'Interim n-reestimation
+# n-of-1 trial.R' to all the rows of x
 initial <- apply(x, 1, reestim)
+ 
+# Make 'initial' a matrix so that it can be combined with x
+initial <- as.matrix(initial, round = 2)
+
+# Combine x and 'initial'
+results <- cbind(x, initial)
+
+
+
+
+
+
+
+
+
+
+
+
+
+

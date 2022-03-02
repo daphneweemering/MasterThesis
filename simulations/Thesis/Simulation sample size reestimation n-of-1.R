e0b954fe11923b3853db0c894cbb98f76129c9d3
daphneweem <dnweemering@gmail.com>
2022-03-02 15:21:07 +0100

    Finalized for running simulations

--- simulations/Thesis/Simulation sample size reestimation n-of-1.R
+++ simulations/Thesis/Simulation sample size reestimation n-of-1.R
@@ -15,17 +15,32 @@ fn <- c(0.25, 0.5, 0.75)
 # 'hvar_error', 'tvar_treatment', tvar_error' and 'fn'
 x <- expand.grid(hvar_treatment, hvar_error, tvar_treatment, tvar_error, fn)
 
-x1 <- x[-c(1,2,3,4,5,6,7,8,9,10),]
-
 # Use apply to use the function 'reestim' from the file 'Interim n-reestimation
 # n-of-1 trial.R' to all the rows of x
-initial <- apply(x, 1, reestim)
- 
-# Make 'initial' a matrix so that it can be combined with x
-initial <- as.matrix(initial, round = 2)
+initial <- apply(x, 1, reestim)  
+
+# Unlist 'initial'
+temp <- t(as.data.frame(lapply(initial, unlist)))
 
 # Combine x and 'initial'
-results <- cbind(x, initial)
+results <- cbind(x, temp)
+
+# Remove the long rownames from 'results'
+rownames(results) <- c()
+
+# Change the column names for the 'initial' matrix (for interpretability)
+names(results)[1] <- 'hyp_psi'
+names(results)[2] <- 'hyp_sigma'
+names(results)[3] <- 'true_psi'
+names(results)[4] <- 'true_sigma'
+names(results)[5] <- 'fraction'
+names(results)[6] <- 'power'
+names(results)[7] <- 'initsampsize'
+names(results)[8] <- 'f_initsampsize'
+names(results)[9] <- 'reestimsampsize'
+names(results)[10] <- 'final_minus_initial'
+
+
 
 
 
